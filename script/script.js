13827618162ebf1b6b1d75815e382b6b351ca20e
IKEAman0621 <scouten0621@outlook.com>
2025-05-09 07:57:23 +0200

    many mor changes

--- script/script.js
+++ script/script.js
@@ -43,3 +43,11 @@ function updateImages() {
   function toggleSideNav(){ 
     sideNav.classList.toggle("open")
   }
+
+
+
+  
+  function toggleContent(id) {
+    const content = document.getElementById(id);
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  }
